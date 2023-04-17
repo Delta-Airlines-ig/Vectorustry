9aabd40a330c6f72b3c2117bd96ec0d43f46e54f
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-17 13:36:16 -0400

    Update IdkMod.java

--- src/idk/IdkMod.java
+++ src/idk/IdkMod.java
@@ -2,7 +2,7 @@ package idk;
 
 import arc.*;
 import arc.util.*;
-import idk.content.EarthItems;
+import idk.content.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
