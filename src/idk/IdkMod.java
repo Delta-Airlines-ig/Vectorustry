ea2abea9ce58de270d20f3723dcfcac69a5d5af8
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-17 13:27:04 -0400

    Update IdkMod.java

--- src/idk/IdkMod.java
+++ src/idk/IdkMod.java
@@ -31,7 +31,7 @@ public class IdkMod extends Mod{
     }
 
 //here?
-    @Override
+    //@Override
     public void loadContent(){
         Log.info("Loading some content i totally made myself.");
               //  EarthLiquids.load();
