593eb70c8a550f81c49d14c97dcd8e201f208e66
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-17 08:46:12 -0400

    Update EarthItems.java

--- src/idk/content/EarthItems.java
+++ src/idk/content/EarthItems.java
@@ -12,7 +12,7 @@ public class EarthItems{
   //add voltite alloys
 
     public static void load(){
-        voltite = new Item("voltite", Color.valueOf("00f0ec")){
+        voltite = new Item("voltite", Color.valueOf("00f0ec")){{
                 //description = "seemingly magical substance. holds alot of charge and will react violently with itself in certain conditions. when alloyed with metals, they become far more durable for some reason. this was discovered hundreds of years ago but was never published for unknown reasons, we only know this because of some old documents found aboard the spacecraft that crashed on Serpulo.";
                 //charge = 9.5f;
                 //radioactivity = 0f;
@@ -20,7 +20,7 @@ public class EarthItems{
                 //flammability = 0f;
                 //cost = 0.1f;
                 //hardness = 15;
-        };
+        }};
 
 
       
