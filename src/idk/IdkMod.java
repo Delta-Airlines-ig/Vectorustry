a46b514bf16975369f0cc8ec827d3cbf8b62daa9
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-17 12:25:49 -0400

    Update IdkMod.java

--- src/idk/IdkMod.java
+++ src/idk/IdkMod.java
@@ -29,17 +29,7 @@ public class IdkMod extends Mod{
         });
     }
 
-    @Override
-    public void loadContent(){
-        Log.info("Loading some content i totally made myself.");
-              //  EarthLiquids.load();
-        EarthItems.load();
-        EarthBlocks.load();
-        EarthPlanets.load();
-     //   EarthSectors.load();
-     //   EarthTechTree.load();
-        
-    }
+//here?
 
 }
 //i have no clue what im doing i just stole this from some other mods
@@ -54,4 +44,14 @@ public class IdkMod extends Mod{
         
    // }
 //}
-
+    @Override
+    public void loadContent(){
+        Log.info("Loading some content i totally made myself.");
+              //  EarthLiquids.load();
+        EarthItems.load();
+        EarthBlocks.load();
+        EarthPlanets.load();
+     //   EarthSectors.load();
+     //   EarthTechTree.load();
+        
+    }
