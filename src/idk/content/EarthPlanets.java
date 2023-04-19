887309c3333001c03042895584451ca0acc53375
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-19 11:44:08 -0400

    Update EarthPlanets.java

--- src/idk/content/EarthPlanets.java
+++ src/idk/content/EarthPlanets.java
@@ -10,13 +10,13 @@ import mindustry.graphics.g3d.*;
 import mindustry.type.*;
 
 public class EarthPlanets{
-    public static Planet earth;
+    public static Planet earth, placeholder-moon;
 
     public static void load(){
         try{
           //placeholder name ig idk a good planet name
             earth = new Planet("earth?", Planets.sun, 1f, 2) {{
-		    localizedName = "placeholder name ig";
+		    localizedName = "ohio";
 		    	icon = "router";
 			iconColor = Color.red;
                 bloom = true;
@@ -53,6 +53,53 @@ public class EarthPlanets{
 				teamRule.infiniteAmmo = teamRule.infiniteResources = true;
 			};
                  generator = new placeholdergen();
+                meshLoader = () -> new HexMesh(this, 4);
+                startSector = 1;
+                sectorSeed = -1;
+                //defaultCore = fortress;
+                defaultCore = Blocks.coreShard;
+                
+            }};
+		placeholder-moon = new Planet("placeholdermoon", EarthPlanets.earth, 0.25f, 2) {{
+		    localizedName = "kansas";
+		    	icon = "router";
+			rotateTime = 1f;
+			orbitRadius = 30f;
+			iconColor = Color.blue;
+                bloom = true;
+			visible = true;
+			accessible = true;
+			hasAtmosphere = false;
+			alwaysUnlocked = true;
+                //atmosphereColor = Color.gray.cpy();
+                //landCloudColor = Color.clear.cpy();
+              //  atmosphereRadOut = 0.5f;
+              //  atmosphereRadIn = 0.05f;
+               // tidalLock = true;
+               // hasAtmosphere = true;
+              //  generator = new idkGenerator();
+                //placeholder
+                ruleSetter = r -> {
+				r.hideBannedBlocks = true;
+				r.waveTeam = Team.crux;
+				r.placeRangeCheck = false;
+				r.showSpawns = true;
+				r.waveSpacing = 15 * Time.toSeconds;
+				r.initialWaveSpacing = 150f * Time.toMinutes;
+				if(r.sector.preset == null)r.winWave = 150;
+				r.coreDestroyClear = true;
+				
+				Rules.TeamRule teamRule = r.teams.get(r.defaultTeam);
+				teamRule.rtsAi = true;
+				teamRule.unitBuildSpeedMultiplier = 10f;
+				teamRule.blockDamageMultiplier = 2f;
+				teamRule.buildSpeedMultiplier = 3f;
+				teamRule.blockHealthMultiplier = 1.75f;
+				
+				teamRule = r.teams.get(r.waveTeam);
+				teamRule.infiniteAmmo = teamRule.infiniteResources = true;
+			};
+                 generator = new placeholdergen2();
                 meshLoader = () -> new HexMesh(this, 4);
                 startSector = 1;
                 sectorSeed = -1;
