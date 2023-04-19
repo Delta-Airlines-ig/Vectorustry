0de0cfb48d01f9fb6eb77ea9c04f7f15ea7fae0c
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-19 10:13:40 -0400

    Update EarthItems.java

--- src/idk/content/EarthItems.java
+++ src/idk/content/EarthItems.java
@@ -17,8 +17,9 @@ public class EarthItems{
 //static here? \/
     public static void load(){
         voltite = new Item("voltite", Color.valueOf("00f0ec")){{
+            localizedName = "voltite";
                 //description = "seemingly magical substance. holds alot of charge and will react violently with itself in certain conditions. when alloyed with metals, they become far more durable for some reason. this was discovered hundreds of years ago but was never published for unknown reasons, we only know this because of some old documents found aboard the spacecraft that crashed on Serpulo.";
-                //charge = 9.5f;
+                charge = 9.5f;
                 //radioactivity = 0f;
                 //explosiveness = 1f;
                 //flammability = 0f;
@@ -29,12 +30,14 @@ public class EarthItems{
 
       
         iron = new Item("iron"){{
+            localizedName = "iron";
             flammability = 0f;
             explosiveness = 0f;
             hardness = 0;
         }};
 
         lithium = new Item("lithium"){{
+            localizedName = "lithium";
             flammability = 0.1f;
             explosiveness = 0.3f;
             charge = 0.75f;
@@ -42,19 +45,23 @@ public class EarthItems{
         }};
 
         tin = new Item("tin"){{
+            localizedName = "tin";
             explosiveness = 0f;
             hardness = 1;
         }};
 
         stone = new Item("stone"){{
+            localizedName = "stone";
             hardness = 0;
         }};
 
         steel = new Item("steel", Color.valueOf("ec83af")){{
+            localizedName = "steel";
             flammability = 0f;
             cost = 2;
         }};
         aluminum = new Item("aluminum", Color.valueOf("ec83af")){{
+            localizedName = "aluminum";
             flammability = 0f;
             cost = 1;
         }};
