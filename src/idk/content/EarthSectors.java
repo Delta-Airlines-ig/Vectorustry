3181efa5efa795bfb06c39f4282b2ab9bb9cafeb
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-19 08:09:17 -0400

    Create EarthSectors.java

---
+++ src/idk/content/EarthSectors.java
@@ -0,0 +1,22 @@
+package newhorizon.content;
+
+import mindustry.type.SectorPreset;
+
+public class Earthsectors{
+	public static SectorPreset Reentry;
+	
+	public static void load(){
+		Reentry = new SectorPreset("Reentry", EarthPlanets.earth, 1){{
+			captureWave = 40;
+			difficulty = 5;
+			
+			alwaysUnlocked = true;
+			
+			//rules = r -> {
+			//	r.winWave = captureWave;
+			//};
+		}};
+		
+		
+	}
+}
