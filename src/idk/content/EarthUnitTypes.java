e368b1c6ef9a558309bf3f39f238a12b7d940452
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-20 11:07:18 -0400

    Update EarthUnitTypes.java

--- src/idk/content/EarthUnitTypes.java
+++ src/idk/content/EarthUnitTypes.java
@@ -94,7 +94,7 @@ public static void load() {
                 }};
             }});
         }};
-	//done almost
+	//done
 	MCLW = new UnitType("MCLW"){{
             speed = 0.8f;
             hitSize = 8f;
@@ -113,13 +113,13 @@ public static void load() {
                 }};
             }});
         }};
-	//done almost
+	//done
 	MCLWA = new UnitType("MCLWA"){{
             speed = 0.75f;
             hitSize = 8f;
             health = 600;
 	    armor = 6;
-            weapons.add(new Weapon("laser-cannon"){{
+            weapons.add(new Weapon("cannon"){{
                 reload = 60f;
                 x = 6f;
                 y = 2f;
@@ -132,19 +132,19 @@ public static void load() {
                 }};
             }});
         }};
-	//done almost
+	//done
 	HCLW = new UnitType("HCLW"){{
             speed = 0.4f;
             hitSize = 8f;
             health = 1000;
 	    armor = 3;
-            weapons.add(new Weapon("guided-missiles"){{
+            weapons.add(new Weapon("cannon"){{
                 reload = 60f;
                 x = 6f;
                 y = 2f;
                 top = false;
                // ejectEffect = Fx.casing1;
-                bullet = new MissileBulletType(7f, 25){{
+                bullet = new BasicBulletType(7f, 25){{
                     width = 7f;
                     height = 9f;
                     lifetime = 60f;
@@ -152,6 +152,101 @@ public static void load() {
             }});
         }};
 
+	//done
+	HCLWA = new UnitType("HCLWA"){{
+            speed = 0.4f;
+            hitSize = 8f;
+            health = 1000;
+	    armor = 6;
+            weapons.add(new Weapon("cannon"){{
+                reload = 60f;
+                x = 6f;
+                y = 2f;
+                top = false;
+               // ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(7f, 25){{
+                    width = 7f;
+                    height = 9f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
+	//done
+	LCMW = new UnitType("LCMW"){{
+            speed = 1f;
+            hitSize = 8f;
+            health = 250;
+	    armor = 3;
+            weapons.add(new Weapon("laser-cannon"){{
+                reload = 40f;
+                x = 4f;
+                y = 2f;
+                top = false;
+               // ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(5f, 50){{
+                    width = 14f;
+                    height = 18f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
+	//done
+	LCMWA = new UnitType("LCMWA"){{
+            speed = 0.95f;
+            hitSize = 8f;
+            health = 250;
+	    armor = 6;
+            weapons.add(new Weapon("laser-cannon"){{
+                reload = 40f;
+                x = 4f;
+                y = 2f;
+                top = false;
+               // ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(5f, 50){{
+                    width = 14f;
+                    height = 18f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
+	//done
+	MCMW = new UnitType("MCMW"){{
+            speed = 0.8f;
+            hitSize = 8f;
+            health = 600;
+	    armor = 3;
+            weapons.add(new Weapon("fast-laser-cannon"){{
+                reload = 25f;
+                x = 6f;
+                y = 2f;
+                top = false;
+               // ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(5f, 45){{
+                    width = 14f;
+                    height = 18f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
+	//done
+	MCMW = new UnitType("MCMW"){{
+            speed = 0.75f;
+            hitSize = 8f;
+            health = 600;
+	    armor = 6;
+            weapons.add(new Weapon("fast-laser-cannon"){{
+                reload = 25f;
+                x = 6f;
+                y = 2f;
+                top = false;
+               // ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(5f, 45){{
+                    width = 14f;
+                    height = 18f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
   
   
 }
