fa74c0506eb5bafe4b34948af992631c0fd6116f
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-20 14:08:48 -0400

    Update EarthUnitTypes.java

--- src/idk/content/EarthUnitTypes.java
+++ src/idk/content/EarthUnitTypes.java
@@ -50,7 +50,7 @@ public static void load() {
            //     y = 2f;
             //    top = false;
                // ejectEffect = Fx.casing1;
-               bullet = new BasicBulletType(2.5f, 9)// {{
+               bullet = new BasicBulletType(2.5f, 9); // {{
                  //   width = 7f;
                  //   height = 9f;
                   //  lifetime = 60f;
