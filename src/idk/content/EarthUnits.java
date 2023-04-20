16061545577717246fd670592551eb9b6f39918a
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-20 07:15:05 -0400

    hopefully this works

---
+++ src/idk/content/EarthUnits.java
@@ -0,0 +1,136 @@
+package idk.content;
+
+import arc.Core;
+import arc.func.Cons;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.Angles;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.math.Rand;
+import arc.math.geom.Rect;
+import arc.math.geom.Vec2;
+import arc.struct.ObjectSet;
+import arc.struct.Seq;
+import arc.util.Time;
+import arc.util.Tmp;
+import mindustry.Vars;
+import mindustry.ai.UnitCommand;
+import mindustry.ai.types.BuilderAI;
+import mindustry.ai.types.MinerAI;
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.content.StatusEffects;
+import mindustry.entities.Damage;
+import mindustry.entities.Effect;
+import mindustry.entities.Lightning;
+import mindustry.entities.Units;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.MultiEffect;
+import mindustry.entities.part.HaloPart;
+import mindustry.entities.part.RegionPart;
+import mindustry.entities.part.ShapePart;
+import mindustry.entities.pattern.*;
+import mindustry.entities.units.WeaponMount;
+import mindustry.game.Team;
+import mindustry.gen.*;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.MultiPacker;
+import mindustry.graphics.Pal;
+import mindustry.type.ItemStack;
+import mindustry.type.StatusEffect;
+import mindustry.type.UnitType;
+import mindustry.type.Weapon;
+import mindustry.type.ammo.ItemAmmoType;
+import mindustry.type.ammo.PowerAmmoType;
+import mindustry.type.weapons.PointDefenseWeapon;
+import mindustry.type.weapons.RepairBeamWeapon;
+import mindustry.world.meta.BlockFlag;
+import mindustry.world.meta.Env;
+
+import static arc.graphics.g2d.Draw.color;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.randLenVectors;
+import static mindustry.Vars.tilePayload;
+import static mindustry.Vars.tilesize;
+
+public class EarthUnits{
+
+			
+	public static NHUnitType
+			
+    
+			sharp;
+
+
+public static void load() {
+  
+sharp = new UnitType("sharp"){{
+			outlineColor = OColor;
+			constructor = EntityMapping.map(3);
+
+			itemCapacity = 15;
+			health = 140;
+			armor = 1;
+			engineOffset = 10F;
+			engineSize = 2.8f;
+			speed = 1.5f;
+			flying = true;
+			accel = 0.08F;
+			drag = 0.02f;
+			baseRotateSpeed = 1.5f;
+			rotateSpeed = 2.5f;
+			hitSize = 10f;
+			singleTarget = true;
+
+			weapons.add(new Weapon(){{
+				top = false;
+				rotate = false;
+				alternate = false;
+				mirror = false;
+				x = 0f;
+				y = 0f;
+				reload = 30f;
+				shoot = new ShootHelix(){{
+					shots = 4;
+					shotDelay = 4f;
+				}};
+				inaccuracy = 5f;
+				ejectEffect = Fx.none;
+				velocityRnd = 0.125f;
+				shake = 2f;
+				maxRange = 140f;
+				bullet = new BasicBulletType(3.5f, 6f){{
+					trailWidth = 1f;
+					trailLength = 10;
+					drawSize = 200f;
+
+					homingPower = 0.1f;
+					homingRange = 120f;
+					width = 5f;
+					height = 25f;
+					keepVelocity = true;
+					knockback = 0.75f;
+					trailColor = backColor = lightColor = lightningColor = hitColor = NHColor.lightSkyBack;
+					frontColor = backColor.cpy().lerp(Color.white, 0.45f);
+					trailChance = 0.1f;
+					trailParam = 1f;
+					trailEffect = NHFx.trailToGray;
+					despawnEffect = NHFx.square(backColor, 18f, 2, 12f, 2);
+					hitEffect = NHFx.lightningHitSmall(backColor);
+					shootEffect = NHFx.shootLineSmall(backColor);
+					smokeEffect = Fx.shootBigSmoke2;
+				}};
+				shootSound = NHSounds.thermoShoot;
+			}});
+		
+}};
+  
+  
+}
+}
