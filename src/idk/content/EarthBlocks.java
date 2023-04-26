c940c681ae831c515a1f21cc43c1c0e4ffdbbe65
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-26 08:29:32 -0400

    Update EarthBlocks.java

--- src/idk/content/EarthBlocks.java
+++ src/idk/content/EarthBlocks.java
@@ -38,7 +38,7 @@ import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 import mindustry.content.*;
 import idk.content.*;
-//import idk.world.blocks.storage.*;
+import idk.world.blocks.storage.*;
 
 import static idk.content.EarthLiquids.*;
 import static idk.content.EarthUnitTypes.*;
@@ -147,7 +147,7 @@ public class EarthBlocks{
             variants = 2;
         }};
         //cores and stuff
-        fortress = new CoreBlock("core-fortress"){{
+        fortress = new DropCoreBlock("core-fortress"){{
             requirements(Category.effect, BuildVisibility.editorOnly, with(Items.copper, 1000, Items.lead, 800));
             alwaysUnlocked = true;
 
@@ -161,7 +161,7 @@ public class EarthBlocks{
             public CoreBuild(thrusterTime = 0f);
         }};
 
-        stronghold = new CoreBlock("core-stronghold"){{
+        stronghold = new DropCoreBlock("core-stronghold"){{
             requirements(Category.effect, with(Items.copper, 3000, Items.lead, 5000, Items.silicon, 3000, EarthItems.iron, 5000));
 
             unitType = EarthUnitTypes.theta;
@@ -175,7 +175,7 @@ public class EarthBlocks{
             public CoreBuild(thrusterTime = 0f);
         }};
 
-        bunker = new CoreBlock("core-bunker"){{
+        bunker = new DropCoreBlock("core-bunker"){{
             requirements(Category.effect, with(Items.copper, 8000, Items.lead, 8000, Items.silicon, 5000, EarthItems.steel, 4000, EarthItems.lithium, 6000, EarthItems.aluminum, 5000, EarthItems.iron, 5000));
 
             unitType = EarthUnitTypes.zeta;
