526267f8c6337b8cac07b5ea8f5f1f5daa5ce0ed
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-04-26 23:37:01 -0400

    Update EarthSectors.java

--- src/idk/content/EarthSectors.java
+++ src/idk/content/EarthSectors.java
@@ -3,10 +3,20 @@ import idk.content.*;
 import mindustry.type.SectorPreset;
 
 public class EarthSectors{
-	public static SectorPreset Reentry;
+	public static SectorPreset Reentry, placeholdersector;
 	
 	public static void load(){
-		Reentry = new SectorPreset("Reentry", EarthPlanets.earth, 1){{
+		//Reentry = new SectorPreset("Reentry", EarthPlanets.earth, 1){{
+		//	captureWave = 40;
+		//	difficulty = 5;
+			
+		//	alwaysUnlocked = true;
+			
+			//rules = r -> {
+			//	r.winWave = captureWave;
+			//};
+		//}};
+				placeholdersector = new SectorPreset("placeholdersector", EarthPlanets.earth, 1){{
 			captureWave = 40;
 			difficulty = 5;
 			
