9cf873f748b9f327a0d6fc2bb2f70a6c0bca7245
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-05-03 17:59:19 -0400

    Create EarthSounds.java

---
+++ src/idk/content/EarthSounds.java
@@ -0,0 +1,43 @@
+package idk.content;
+
+import arc.*;
+import arc.assets.*;
+import arc.assets.loaders.*;
+import arc.audio.*;
+import mindustry.*;
+
+public class EarthSounds {
+    public static Sound RadioAmbent, RadioPlace, NukeLaunch, NukeHit;
+    public static final String[] soundFiles = {"RadioAmbent", "RadioPlace", "NukeLaunch", "NukeHit"};
+    private static int num = 0;
+
+    public static void load() {
+        num = 0;
+        pistonPush = l();
+        pistonPull = l();
+        presentBells = l();
+        boost = l();
+        tntfuse = l();
+        boing = l();
+    }
+    protected static Sound l() {
+        return loadSound(soundFiles[num++]);
+    }
+
+//totally not stolen from betamindy
+    protected static Sound loadSound(String soundName) {
+        if(!Vars.headless) {
+            String name = "sounds/" + soundName;
+            String path = name + ".ogg";
+
+            Sound sound = new Sound();
+
+            AssetDescriptor<?> desc = Core.assets.load(path, Sound.class, new SoundLoader.SoundParameter(sound));
+            desc.errored = Throwable::printStackTrace;
+
+            return sound;
+        } else {
+            return new Sound();
+        }
+    }
+}
