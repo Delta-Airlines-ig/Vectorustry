33b19a7a9fc6fd6464ea0e27bf9703ef94b1ff8d
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-05-04 13:51:28 -0400

    Update EarthTechtree.java

--- src/idk/content/EarthTechtree.java
+++ src/idk/content/EarthTechtree.java
@@ -96,7 +96,7 @@ public class EarthTechtree{
                     });
                 });
             });//turrets
-       /*                             node(EarthBlocks.carbonsequestrator, () -> {
+                                    node(EarthBlocks.carbonsequestrator, () -> {
             node(EarthBlocks.carboncatalyst, () -> {
             node(EarthBlocks.steelSmelter, () -> {
             
